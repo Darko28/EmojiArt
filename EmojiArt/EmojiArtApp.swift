ed1a95eb0f92d8bf9c86fba31971e04eb6ef64cd
DarkoBomer <qq1572055493@icloud.com>
2020-10-26 03:32:38 +0800

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Darko on 10/26/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
